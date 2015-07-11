3da540aece3453493b12d28225d4513bde910222
Jerry D'Antonio <stumpjumper@gmail.com>
2015-07-11 11:57:18 -0400

    Renamed repo to match accepted Golang conventions.

--- mathapp/main.go
+++ mathapp/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/jdantonio/examples.go/hello"
-	"github.com/jdantonio/examples.go/mymath"
+	"github.com/jdantonio/examples-go/hello"
+	"github.com/jdantonio/examples-go/mymath"
 )
 
 func main() {
